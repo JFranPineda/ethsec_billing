cbbc03946f1761b8d85f64efed779f4d7b0623e4
JFranPineda <jpinedas@uni.pe>
2024-02-15 14:49:06 -0500

    Fixed API_URL

--- src/store/storeTypes.js
+++ src/store/storeTypes.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://light-pink-angelfish.cyclic.app/";
-export const API_URL = "http://localhost:3000/";
+export const API_URL = "https://light-pink-angelfish.cyclic.app/";
+// export const API_URL = "http://localhost:3000/";
